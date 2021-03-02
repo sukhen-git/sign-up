17ae3a472cf41bedf807257cf5813e50aae2ac29
bhattacharjee.sukhen@gmail.com <bhattacharjee.sukhen@gmail.com>
2021-03-02 11:10:00 +0530

    'initial-commit'

---
+++ src/main/java/com/app/ttd/ui/LdapSignUpController.java
@@ -0,0 +1,81 @@
+package com.app.ttd.ui;
+
+import static java.util.Comparator.comparing;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.app.ttd.repository.LdapSignupRepository;
+import com.app.ttd.util.LdapDepartments;
+import com.app.ttd.util.LdapRolse;
+import com.app.ttd.util.SignupObject;
+
+@RestController
+@RequestMapping("/ldapSignUp")
+public class LdapSignUpController {
+
+	@Autowired
+	private LdapSignupRepository repository;
+	
+	private boolean checkUserExist(String uid) {
+		boolean success=false;
+		if(uid!=null && uid!="") {
+		List<String> getuid=repository.getUID(uid);
+		if(getuid!=null && getuid.size()!=0) {
+			success=true;;
+		}else {
+			success=false;
+		}
+		}else {
+			success=false;
+		}
+		return success;
+	}
+	
+	@PostMapping(path = "/signup")
+	public ResponseEntity<String> userSignUp(@RequestBody SignupObject usr)  {
+		
+		if(checkUserExist(usr.getUid())) {
+			return new ResponseEntity<String>("User id Already Exists....", HttpStatus.BAD_REQUEST);
+		} else {
+			repository.createUser(usr);
+			repository.addMemberToGroup("IT","IT_ADMIN", usr);
+			return new ResponseEntity<String>("user created", HttpStatus.OK);
+		}
+	}
+	
+	@PostMapping(path="/updatePassword")
+	public ResponseEntity<String> updatePassword(@RequestParam("emailId")String emailId,@RequestParam("newPassword")String newPassword){
+		if(emailId!=null && emailId!="" && newPassword!=null && newPassword!="") {
+			String passwordUpdate=repository.changePassword(emailId, newPassword);
+			return new ResponseEntity<String>(passwordUpdate, HttpStatus.OK);
+		}else {
+			return new ResponseEntity<String>("Please check the request....", HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@GetMapping(path = "/getAllDepartment", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<LdapDepartments>> getDepartments() {
+		List<LdapDepartments> getDepartments = repository.getAllDepartment();
+		Collections.sort(getDepartments, comparing(LdapDepartments::getDepartmentName));
+		return new ResponseEntity<List<LdapDepartments>>(getDepartments, HttpStatus.OK);
+
+	}
+
+	@GetMapping(path = "/getAllRoles", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<LdapRolse>> getAllRoles() {
+		List<LdapRolse> getRoles = repository.getAllRoles();
+		Collections.sort(getRoles, comparing(LdapRolse::getRoleNames));
+		return new ResponseEntity<List<LdapRolse>>(getRoles, HttpStatus.OK);
+	}
+}
